0d0049ddcd29ed98107c5889c9cc60e7c4f8a352
bearrabi <bearrabi0506@gmail.com>
2020-05-03 01:00:08 +0000

    create/seeder WeightsTable for test weights table

--- database/seeds/WeightsTablesSeeder.php
+++ database/seeds/WeightsTablesSeeder.php
@@ -11,6 +11,17 @@ class WeightsTablesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('weights')->insert([
+            [
+                'user_id' => 1,
+                'weight' => 96.5,
+                'measure_dt' => date('2020-05-03 10:00:00')
+            ],
+            [
+                'user_id' => 1,
+                'weight' => 96.7,
+                'measure_dt' => date('2020-05-04 11:00:00')
+            ]
+        ]);
     }
 }
