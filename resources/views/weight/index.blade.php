1431080449aa2eaee4c1371eb287e3966028909c
bearrabi <bearrabi0506@gmail.com>
2020-05-04 00:32:28 +0000

    edit/view weight index for check action weight index

---
+++ resources/views/weight/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('weight index') }}</div>
+
+                <div class="card-body">
+                    <div class="alert alert-primary"> weight index page</div>         
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
