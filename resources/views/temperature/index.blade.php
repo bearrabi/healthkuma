d573d3a533e6f97e67a555a4f7abb8d504524698
bearrabi <bearrabi0506@gmail.com>
2020-05-04 01:21:39 +0000

    edit/view temperature index for check indec action

--- resources/views/temperature/index.blade.php
+++ resources/views/temperature/index.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('weight index') }}</div>
+                <div class="card-header">{{ __('temperature index') }}</div>
 
                 <div class="card-body">
-                    <div class="alert alert-primary"> weight index page</div>         
+                    <div class="alert alert-secondary"> temperature index page</div>         
                 </div>
             </div>
         </div>
