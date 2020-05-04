2938d46819c73a5babe7920a23db7a83217ea3a8
bearrabi <bearrabi0506@gmail.com>
2020-05-04 01:15:53 +0000

    create/view temperature for new view

---
+++ resources/views/temperature/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('weight edit') }}</div>
+
+                <div class="card-body">
+                    <div class="alert alert-primary"> weight edit page</div>         
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
