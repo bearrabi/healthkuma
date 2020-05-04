8f728b4213553e83826f8cfc19ff02d9082bbe9a
bearrabi <bearrabi0506@gmail.com>
2020-05-04 01:22:03 +0000

    edit/view temperature create for check create action

--- resources/views/temperature/create.blade.php
+++ resources/views/temperature/create.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('weight create') }}</div>
+                <div class="card-header">{{ __('temperature create') }}</div>
 
                 <div class="card-body">
-                    <div class="alert alert-primary"> weight create page</div>         
+                    <div class="alert alert-secondary"> temperature create page</div>         
                 </div>
             </div>
         </div>
