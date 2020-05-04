aa73159504f3796e2b0ea648a4371b62e7ddae32
bearrabi <bearrabi0506@gmail.com>
2020-05-04 07:42:23 +0000

    edit/view app for add delete check js function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,15 @@
             @yield('content')
         </main>
     </div>
+
+    <script>
+        function delete_alert(e){
+        if(!window.confirm('本当に削除しても大丈夫ですか？')){
+            window.alert('キャンセルしました');
+            return false;
+        }
+        document.deleteform.submit();
+        }
+    </script>
 </body>
 </html>
