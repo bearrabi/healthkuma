26722ec8cebeea95a1abd3f1b740388827c74378
bearrabi <bearrabi0506@gmail.com>
2020-05-04 00:32:48 +0000

    edit/view weight create for check action create index

---
+++ resources/views/weight/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('weight create') }}</div>
+
+                <div class="card-body">
+                    <div class="alert alert-primary"> weight create page</div>         
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
