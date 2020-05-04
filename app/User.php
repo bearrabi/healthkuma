0a1a8edcb0f4814f9d064c298dc61c55e767c4b4
bearrabi <bearrabi0506@gmail.com>
2020-05-04 06:34:57 +0000

    edit/model user for add relation hasmany weights and temperature

--- app/User.php
+++ app/User.php
@@ -36,4 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function temperatures(){ return $this->hasMany('App\Temperature');   }
+    public function weights(){ return $this->hasMany('App\Weight');   }
 }
