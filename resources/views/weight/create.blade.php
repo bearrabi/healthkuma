e5bdeb8a31d075207671573967af687ea162e9ac
bearrabi <bearrabi0506@gmail.com>
2020-05-05 09:24:50 +0000

    edit/view weight create for change from txtbox to selectbox

--- resources/views/weight/create.blade.php
+++ resources/views/weight/create.blade.php
@@ -1,17 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+.txtbx,.selectbx{margin: 0 5px 0 25px;}
+#dot{ 
+  font-weight: bold;
+  font-size: 24px;
+}
+</style>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('weight create') }}</div>
+  <div class="row">
+    <form method="POST" action="{{ route('weight.store') }}">
+    @csrf
+      <!--日付-->
+      <div class="form-group">
+        <label for="year">日付</label>
+      </div>
+      <div class="form-inline">
+        <select name="year" id="year" class="form-control selectbx">
+            @foreach($years as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>年
+        <select name="month" id="month" class="form-control selectbx">
+            @foreach($months as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>月
+        <select name="day" id="day" class="form-control selectbx">
+            @foreach($days as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>日
+      </div>
+      <br><br>
 
-                <div class="card-body">
-                    <div class="alert alert-primary"> weight create page</div>         
-                </div>
-            </div>
-        </div>
-    </div>
+      <!--時刻-->
+      <div class="form-group">
+        <label for="hour">時刻</label>
+      </div>
+      <div class="form-inline">
+      <select name="year" id="hour" class="form-control selectbx">
+            @foreach($hours as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>時
+        <select name="minute" id="minute" class="form-control selectbx">
+            @foreach($minutes as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>分
+        <select name="second" id="second" class="form-control selectbx">
+            @foreach($seconds as $key => $value)
+              @if ($value == true)
+                <option value="{{$key}}" selected>{{$key}}</option>
+              @else
+                <option value="{{$key}}">{{$key}}</option>
+              @endif
+            @endforeach
+        </select>秒
+        
+      </div>
+      <br><br>
+
+      <!--体重-->
+      <div class="form-group">
+        <label for="weight1">体重</label>
+      </div>
+      <div class="form-inline">
+        <input name="weight1" type="text" class="form-control txtbx" id="weight1"><span id="dot">.</span>
+        <input name="weight2" type="text" class="form-control txtbx" id="weight2">kg
+      </div>
+      <br><br>
+
+      <!--更新ボタン-->
+      <input class="btn btn-info" type="submit" value="登録">
+    </form>
+  </div>
 </div>
 @endsection
