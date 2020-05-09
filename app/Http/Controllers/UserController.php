ae3d56a3124c525ca0e1ecba6af60595bca0b0cc
bearrabi <bearrabi0506@gmail.com>
2020-05-09 07:40:16 +0000

    edit/controller user controller for add setting graph data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
     //show grapgh
     public function index()  
     {
-    //get auth ID
+        //get auth ID
         $id = auth()->user()->id;
 
         //get user's weights info
@@ -26,7 +26,35 @@ class UserController extends Controller
 
         //get user's templeture info
         $temps = Temperature::where('user_id', $id)->orderby('measure_dt')->get();
-        return view('user.index', compact('weights','temps'));
+
+        //set graph data
+        $send_views_js_json = array();
+        $index_arr_1d = -1;
+        foreach($weights as $value){
+            
+            //fecth year and month from weights table for create array label name;
+            $json_val_year_month = date('Y/m', strtotime($value->measure_dt));
+
+            //set new year_month to json array
+            $lblnm_year_month = "year_month";
+            if ($index_arr_1d == -1 || 
+                $send_views_js_json[$index_arr_1d][$lblnm_year_month] != $json_val_year_month){
+                $index_arr_1d++;
+                $send_views_js_json[$index_arr_1d] = ['year_month' => $json_val_year_month]; 
+            }
+
+            //set day's info to json array
+            $measure_day    = date('d',strtotime($value->measure_dt));
+            $measure_weight = $value->weight;
+            $arr_day_and_weight = ['day' => $measure_day, 'weight' => $measure_weight];
+            $arr_weight_info = ['weight_info' => $arr_day_and_weight];
+            $send_views_js_json[$index_arr_1d][] = $arr_weight_info;
+        }
+
+        //encode array to json
+        $dates_and_weights_json = json_encode($send_views_js_json);
+
+        return view('user.index', compact('weights','temps','dates_and_weights_json'));
     }
 
     /**
