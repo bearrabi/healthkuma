7fd9a64c0170428e0290df7510e841193dc4cf8c
bearrabi <bearrabi0506@gmail.com>
2020-05-12 11:52:39 +0000

    fix/view weight edit for fix mis code

--- resources/views/weight/edit.blade.php
+++ resources/views/weight/edit.blade.php
@@ -19,7 +19,7 @@
         <label for="year">日付</label>
       </div>
       <div class="form-inline">
-        <input name="year" type="tet" class="form-control txtbx" id="year" value="{{$weights['year']}}" readonly>年 &nbsp;&nbsp;
+        <input name="year" type="text" class="form-control txtbx" id="year" value="{{$weights['year']}}" readonly>年 &nbsp;&nbsp;
         <input name="month" type="text" class="form-control txtbx" id="month" value="{{$weights['month']}}" readonly>月 &nbsp;&nbsp;
         <input name="day" type="text" class="form-control txtbx" id="day"  value="{{$weights['day']}}" readonly>日
       </div>
@@ -30,7 +30,7 @@
         <label for="hour">時刻</label>
       </div>
       <div class="form-inline">
-        <input name="hour" type="tet" class="form-control txtbx" id="hour" value="{{$weights['hour']}}" readonly>時 &nbsp;&nbsp;
+        <input name="hour" type="text" class="form-control txtbx" id="hour" value="{{$weights['hour']}}" readonly>時 &nbsp;&nbsp;
         <input name="minute" type="text" class="form-control txtbx" id="minute" value="{{$weights['minute']}}" readonly>分 &nbsp;&nbsp;
         <input name="second" type="text" class="form-control txtbx" id="second"  value="{{$weights['second']}}" readonly>秒
       </div>
